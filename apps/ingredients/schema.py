5ec4e8f45e4040d98247a8ef1881681e3ddbcaae
mplascencia_cruz <mplascencia.cruz@gmail.com>
2018-11-16 14:02:52 -0600

    Detalle: Category & Ingredient

--- apps/ingredients/schema.py
+++ apps/ingredients/schema.py
@@ -15,11 +15,41 @@ class IngredientType(DjangoObjectType):
         model = Ingredient
 
 class QueryIngredients(object):
+    category = graphene.Field(
+        CategoryType,
+        id=graphene.Int(),
+        name=graphene.String()
+    )
     all_categories = graphene.List(CategoryType)
+
+    ingredient = graphene.Field(
+        IngredientType,
+        id=graphene.Int(),
+        name=graphene.String()
+    )
     all_ingredients = graphene.List(IngredientType)
+
+    def resolve_category(self, info, **kwargs):
+        id = kwargs.get('id', None)
+        name = kwargs.get('name', None)
+
+        if id:
+            return Category.objects.get(pk=id)
+        if name:
+            return Category.objects.get(name=name)
 
     def resolve_all_categories(self, info, **kwargs):
         return Category.objects.all()
 
+    def resolve_ingredient(self, info, **kwargs):
+        id = kwargs.get('id', None)
+        name = kwargs.get('name', None)
+
+        if id:
+            return Ingredient.objects.get(pk=id)
+        if name:
+            return Ingredient.objects.get(name=name)
+
     def resolve_all_ingredients(self, info, **kwargs):
         return Ingredient.objects.select_related('category').all()
+
